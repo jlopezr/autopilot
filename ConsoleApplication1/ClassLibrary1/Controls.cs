2c954e472263ba30dae7279f740f6a9e1f878ee5
SVives <sergio.jose.vives@gmail.com>
2015-02-18 19:00:04 +0100

    Add UDP Test

---
+++ ConsoleApplication1/ClassLibrary1/Controls.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaneLibrary
+{
+    public class Controls
+    {
+        float ail;
+        float elev;
+        float rud;
+        float thr;
+
+        public void set_ail(float a)
+        {
+            ail = a;
+        }
+
+        public float get_ail()
+        {
+            return (ail);
+        }
+
+        public float read_ail()
+        {
+            CommsUDP Ca = new CommsUDP();
+            Controls Con = Ca.ReadControls();
+            return (Con.ail);
+        }
+
+        public void set_elev(float b)
+        {
+            elev = b;
+        }
+
+        public float get_elev()
+        {
+            return (elev);
+        }
+
+        public float read_elev()
+        {
+            CommsUDP Ca = new CommsUDP();
+            Controls Con = Ca.ReadControls();
+            return (Con.elev);
+        }
+
+        public void set_rud(float c)
+        {
+            rud = c;
+        }
+
+        public float get_rud()
+        {
+            return (rud);
+        }
+
+        public float read_rud()
+        {
+            CommsUDP Ca = new CommsUDP();
+            Controls Con = Ca.ReadControls();
+            return (Con.rud);
+        }
+        
+
+        public void set_thr(float d)
+        {
+            thr = d;
+        }
+
+        public float get_thr()
+        {
+            return (thr);
+        }
+
+        public float read_thr()
+        {
+            CommsUDP Ca = new CommsUDP();
+            Controls Con = Ca.ReadControls();
+            return (Con.thr);
+        }
+
+        public Controls(float a, float b, float c, float d)
+        {
+            ail = a;
+            elev = b;
+            rud = c;
+            thr = d;
+        }
+        public Controls()
+        {
+            ail = -999;
+            elev = -999;
+            rud = -999;
+            thr = -999;
+        }
+    }
+}
---
+++ ConsoleApplication1/ClassLibrary1/Situation.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaneLibrary
+{
+    public class Situation
+    {
+        float yaw;
+        float pitch;
+        float roll;
+        float AoA;
+        float speed;
+
+        public void set_yaw(float a) //Ahora Yaw=HDG(true)
+        {
+            yaw = a;
+        }
+
+        public float get_yaw()
+        {
+            return(yaw);
+        }
+
+        public float read_yaw()
+        {
+            CommsUDP C = new CommsUDP();
+            Situation S = C.ReadSituation();
+            return (S.yaw);
+        }
+
+        public void set_pitch(float b)
+        {
+            pitch = b;
+        }
+
+        public float get_pitch()
+        {
+            return(pitch);
+        }
+
+        public float read_pitch()
+        {
+            CommsUDP C = new CommsUDP();
+            Situation S = C.ReadSituation();
+            return (S.pitch);
+        }
+
+        public void set_roll(float c)
+        {
+            roll = c;
+        }
+
+        public float get_roll()
+        {
+            return(roll);
+        }
+
+        public float read_roll()
+        {
+            CommsUDP C = new CommsUDP();
+            Situation S = C.ReadSituation();
+            return (S.roll);
+        }
+
+        public void set_AoA(float d)
+        {
+            AoA = d;
+        }
+
+        public float get_AoA()
+        {
+            return(AoA);
+        }
+
+        public float read_AoA()
+        {
+            CommsUDP C = new CommsUDP();
+            Situation S = C.ReadSituation();
+            return (S.AoA);
+        }
+
+        public void set_speed(float e)
+        {
+            speed = e;
+        }
+
+        public float get_speed()
+        {
+            return(speed);
+        }
+
+        public float read_speed()
+        {
+            CommsUDP C = new CommsUDP();
+            Situation S = C.ReadSituation();
+            return (S.speed);
+        }
+
+        public Situation(float a, float b, float c, float d, float e)
+        {
+            yaw = a;
+            pitch = b;
+            roll = c;
+            AoA = d;
+            speed = e;
+        }
+        public Situation()
+        {
+            
+        }
+    }
+}
