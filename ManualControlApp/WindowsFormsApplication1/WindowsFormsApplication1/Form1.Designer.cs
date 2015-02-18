1d04ca61c777082f286330a83b2341bf6d6d0072
SVives <sergio.jose.vives@gmail.com>
2015-02-18 18:55:36 +0100

    Added App Control Manual

---
+++ ManualControlApp/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(18, 119);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.textBox3.Location = new System.Drawing.Point(18, 158);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "X (-1,1)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Y (-1,1)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Throttle (0,1)";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Location = new System.Drawing.Point(238, 22);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 600);
+            this.panel1.TabIndex = 7;
+            this.panel1.MouseEnter += new System.EventHandler(this.panel1_MouseEnter);
+            this.panel1.MouseLeave += new System.EventHandler(this.panel1_MouseLeave);
+            this.panel1.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseWheel);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 637);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(519, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Click on the panel to start controlling manually, click again to stop the manual " +
+    "controls and center the joystick";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(18, 203);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(61, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Manual";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(65, 300);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 44);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(18, 227);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(214, 17);
+            this.checkBox2.TabIndex = 12;
+            this.checkBox2.Text = "Throttle Keyboard (Number/Arrow Keys)";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(862, 658);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ ManualControlApp/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using XPlane;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public bool Manual;
+        public bool first = true; //first time on manual to start parser and connection
+        private float joyposX; //elevator -1 to 1
+        private float joyposY; //ailerons -1 to 1
+        //private float rudder; //not used
+        private float throttle = 0; //0 to 1
+        XplaneConnection connection = new XplaneConnection();
+        //XplaneParser parser;
+        //parser = new XplaneParser(connection);
+        private bool check2 = false;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            XplanePacketsId.Load(XplaneVersion.Xplane10);
+            //XplaneConnection connection = new XplaneConnection(); 
+            XplaneParser parser = new XplaneParser(connection); //No se puede poner arriba??
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            bool w = SystemInformation.MouseWheelPresent;
+            this.checkBox2.Click += new System.EventHandler(this.checkbox2_Click);
+            this.KeyPreview = true;
+            this.KeyDown += new KeyEventHandler(Form1_KeyPress);
+            if (!w)
+            {
+                check2 = true;
+                this.checkBox2.Checked = check2;
+            }
+            this.textBox3.Text = Convert.ToString(throttle);
+        }
+
+        private void checkbox2_Click(object sender, EventArgs e)
+        {
+            if (!check2)
+            {
+                check2 = true;
+                this.checkBox2.Checked = check2;
+            }
+            else
+            {
+                check2 = false;
+                this.checkBox2.Checked = check2;
+            }
+        }
+
+
+        private void Form1_KeyPress(object sender, KeyEventArgs e)
+        {
+            if (Manual && check2)
+            {
+                Keys key = e.KeyCode;
+                int keyn = (int)e.KeyValue;
+                if (key >= Keys.D0 && key <= Keys.D9)
+                {
+                    keyn = keyn - (int)Keys.D0;
+                    throttle = (float)keyn;
+                    throttle = throttle / 10;
+                }
+                else if (key >= Keys.NumPad0 && key <= Keys.NumPad9)
+                {
+                    keyn = keyn - (int)Keys.NumPad0;
+                    throttle = (float)keyn;
+                    throttle = throttle / 10;
+                }
+                else if (key==Keys.Down||key==Keys.Up)
+                {
+                    if (key == Keys.Up)
+                    {
+                        if (throttle <= 0.99)
+                        {
+                            throttle = throttle + 0.01f;
+                        }
+                        else if (throttle > 0.99)
+                        {
+                            throttle = 1;
+                        }
+                    }
+                    else
+                    {
+                        if (throttle >= 0.01)
+                        {
+                            throttle = throttle - 0.01f;
+                        }
+                        else if (throttle < 0.01)
+                        {
+                            throttle = 0;
+                        }
+                    }
+                    
+                }
+                byte[] cosa = XplanePacketGenerator.JoystickPacket(throttle, joyposX, 0, joyposY);
+                connection.SendPacket(cosa);
+                this.textBox3.Text = Convert.ToString(throttle);
+            }
+        }
+
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            Environment.Exit(Environment.ExitCode);
+        }
+
+        private void panel1_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (Manual)
+            {
+                int mouseX = e.X;
+                int mouseY = e.Y;
+                joyposX = (((float)mouseX) - (this.panel1.Width / 2)) / (this.panel1.Width / 2);
+                joyposY = ((((float)mouseY) - (this.panel1.Height / 2)) / (this.panel1.Height / 2));
+                connection.SendPacket(XplanePacketGenerator.JoystickPacket(throttle, joyposX, 0, joyposY));
+            }
+            this.textBox1.Text = Convert.ToString(joyposX);
+            this.textBox2.Text = Convert.ToString(joyposY);
+
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (Manual)
+            {
+                Manual = false;
+                joyposX = 0;
+                joyposY = 0;
+                this.checkBox1.Checked = Manual;
+                byte[] cosa = XplanePacketGenerator.JoystickPacket(throttle, joyposX, 0, joyposY);
+                connection.SendPacket(cosa); //Center controls
+            }
+            else
+            {
+                Manual = true;
+                this.checkBox1.Checked = Manual;
+                if (first)
+                {
+                    connection.OpenConnections();
+                    XplaneParser parser = new XplaneParser(connection);
+                    parser.Start();
+                }
+                byte[] cosa = XplanePacketGenerator.JoystickPacket(0, 0, 0, 0);
+                connection.SendPacket(cosa); //Center controls
+            }
+        }
+
+        private void panel1_MouseWheel(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (Manual && e.Delta > 0 && throttle <= 0.99)
+            {
+                throttle = throttle + 0.01f;
+            }
+            else if (Manual && e.Delta < 0 && throttle > 0.01f)
+            {
+                throttle = throttle - 0.01f;
+            }
+            else if (Manual && e.Delta < 0 && throttle <= 0.01f)
+            {
+                throttle = 0;
+            }
+            else if (Manual && e.Delta > 0 && throttle > 0.99)
+            {
+                throttle = 1;
+            }
+            byte[] cosa = XplanePacketGenerator.JoystickPacket(throttle, joyposX, 0, joyposY);
+            connection.SendPacket(cosa);
+            this.textBox3.Text = Convert.ToString(throttle);
+        }
+
+        void panel1_MouseLeave(object sender, EventArgs e)
+        {
+            if (panel1.Focused)
+                panel1.Parent.Focus();
+        }
+
+        void panel1_MouseEnter(object sender, EventArgs e)
+        {
+            if (!panel1.Focused)
+                panel1.Focus();
+        }
+
+    }
+}
